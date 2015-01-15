19bcbb7a6984979b9b94c466ff32f403c4635162
Matthew Steglinski <matthew.steglinski@utoronto.ca>
2015-01-14 20:21:35 -0500

    Add /deposit functionality

--- src/java/me/sainttx/banknotes/command/DepositCommand.java
+++ src/java/me/sainttx/banknotes/command/DepositCommand.java
@@ -4,6 +4,8 @@ import me.sainttx.banknotes.BanknotePlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Created by Matthew on 14/01/2015.
@@ -24,6 +26,28 @@ public class DepositCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can deposit bank notes");
+        } else {
+            Player player = (Player) sender;
+            ItemStack item = player.getItemInHand();
+
+            if (item != null && plugin.isBanknote(item)) {
+                double amount = plugin.getBanknoteAmount(item);
+
+                if (amount > 0) {
+                    plugin.getEconomy().depositPlayer(player, amount);
+                    player.sendMessage(plugin.colorMessage(plugin.getConfig().getString("note-redeemed").replace("[money]", plugin.formatDouble(amount))));
+                } else {
+                    player.sendMessage(plugin.colorMessage(plugin.getConfig().getString("invalid-note")));
+                }
+
+                // Remove the item
+                player.getInventory().removeItem(item);
+            } else {
+                player.sendMessage(plugin.colorMessage(plugin.getConfig().getString("nothing-in-hand")));
+            }
+        }
         return false;
     }
 }
